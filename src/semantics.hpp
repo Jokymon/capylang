cfd58fe57a485e8b68b920cf495b7e91febd4f79
Silvan Wegmann <swegmann@narf.ch>
2026-03-02 20:59:50 +0100

    Fix missing override markers

--- src/semantics.hpp
+++ src/semantics.hpp
@@ -16,26 +16,26 @@ public:
 private:
     void append_error_at(source_position location, const std::string& error_message);
 
-    void process(source_range location, node_number& n);
-    void process(source_range location, node_char_literal& n);
-    void process(source_range location, node_bool_const& n);
-    void process(source_range location, node_string_literal& n);
-    void process(source_range location, node_var_reference& n);
-    void process(source_range location, node_pointer_deref& n);
-    void process(source_range location, node_record_definition& n);
-    void process(source_range location, node_record_initialisation& n);
-    void process(source_range location, node_field_deref& n);
-    void process(source_range location, node_function_call& n);
-    void process(source_range location, node_if_expression& n);
-    void process(source_range location, node_while_expression& n);
-    void process(source_range location, node_let_expression& n);
-    void process(source_range location, node_global_definition& n);
-    void process(source_range location, node_function_head& n);
-    void process(source_range location, node_function_definition& n);
-    void process(source_range location, node_cast_expression& n);
-    void process(source_range location, node_expression& n);
-    void process(source_range location, node_import_definition& n);
-    void process(source_range location, node_module& n);
+    void process(source_range location, node_number& n) override;
+    void process(source_range location, node_char_literal& n) override;
+    void process(source_range location, node_bool_const& n) override;
+    void process(source_range location, node_string_literal& n) override;
+    void process(source_range location, node_var_reference& n) override;
+    void process(source_range location, node_pointer_deref& n) override;
+    void process(source_range location, node_record_definition& n) override;
+    void process(source_range location, node_record_initialisation& n) override;
+    void process(source_range location, node_field_deref& n) override;
+    void process(source_range location, node_function_call& n) override;
+    void process(source_range location, node_if_expression& n) override;
+    void process(source_range location, node_while_expression& n) override;
+    void process(source_range location, node_let_expression& n) override;
+    void process(source_range location, node_global_definition& n) override;
+    void process(source_range location, node_function_head& n) override;
+    void process(source_range location, node_function_definition& n) override;
+    void process(source_range location, node_cast_expression& n) override;
+    void process(source_range location, node_expression& n) override;
+    void process(source_range location, node_import_definition& n) override;
+    void process(source_range location, node_module& n) override;
 
     context& parse_context;
 };
